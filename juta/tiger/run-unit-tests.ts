79450495d6019ab17acb01d9d019da213844c84b
Pascal ECHEMANN <developers@flashapi.org>
2017-06-05 13:05:35 +0200

    Minor Update
    Adding the JUTA process duration to the test result message.

--- juta/tiger/run-unit-tests.ts
+++ juta/tiger/run-unit-tests.ts
@@ -24,11 +24,12 @@ tester.process((stats:TestStats)=> {
   else {
     console.log(
 `Test stats:
+- test process duration: ${stats.time}
 - number of test suites: ${stats.numTestSuites}
 - number of disabled test suites: ${stats.numDisabledTestSuites}
 - number of synchronous test cases: ${stats.numTests}
 - number of asynchronous test cases: ${stats.numAsyncTests}
 - number of disabled test cases: ${stats.numDisabledTests}`
-    )
+    );
   }
 });
